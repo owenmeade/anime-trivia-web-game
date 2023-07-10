2f9b438dbfaf377e139a7a57f64a0c3dc90c19b0
Sarah <sarahhawkins26@gmail.com>
2023-07-10 16:15:03 -0700

    testing

--- public/js/script.js
+++ public/js/script.js
@@ -3,10 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   let level = 1;
   let strikes = 0;
   let data;
-const api = "https://opentdb.com/api.php?amount=10&category=31";
-let level = 1;
-const strikes = 0;
-let correctAnswer;
 
   function getTrivia() {
     fetch(api)
@@ -17,21 +13,18 @@ let correctAnswer;
         const question = data.results[level - 1].question;
         const incorrectAnswers = data.results[level - 1].incorrect_answers;
         const correctAnswer = data.results[level - 1].correct_answer;
-        correctAnswer = data.results[level - 1].correct_answer;
-        console.log(data.results)
 
         //Display the question
         document.getElementById("question").innerHTML = question;
-        document.getElementById("question").textContent = question.replaceAll('&quot;', '"');
 
         //Show the answers, including incorrect answers and correct answers
         const answers = [...incorrectAnswers, correctAnswer];
 
         //Shuffle the answers
-        // const shuffleAnswers = shuffle(answers);
+        const shuffleAnswers = shuffle(answers);
 
         //Display answers under choices
-        const choiceContainer = document.getElementById("choices-container");
+        const choiceContainer = document.getElementById("choices");
         choiceContainer.innerHTML = "";
 
         shuffleAnswers.forEach((answer) => {
@@ -40,13 +33,6 @@ let correctAnswer;
           choice.addEventListener("click", () => {
             selectAnswer(choice, correctAnswer);
           });
-        answers.forEach((answer) => {
-          const choice = document.createElement("button");
-          choice.textContent = answer;
-          choice.classList.add("choice");
-          choice.addEventListener("click", (event) => {
-            selectAnswer(event.target, correctAnswer);
-      });
           choiceContainer.appendChild(choice);
         });
 
@@ -60,9 +46,6 @@ let correctAnswer;
           "Failed to fetch trivia questions.";
       });
   }
-      })
-  );
-    } 
 
   //Event listener for start button
   document.getElementById("startButton").addEventListener("click", () => {
@@ -88,21 +71,6 @@ let correctAnswer;
           selectedChoice.classList.remove("selected");
         });
       }
-      choice.classList.add("selected");
-    }
-  }
-//Event listener for answer submission
-document.getElementById("submit").addEventListener("click", validateAnswer);
-
-function selectAnswer(choice, correctAnswer) {
-  if (!choice.classList.contains("selected")) {
-    const selectedChoices = document.querySelectorAll(".choice.selected");
-
-    
-    selectedChoices.forEach((selectedChoice) => {
-        selectedChoice.classList.remove("selected");
-      });
-    
       choice.classList.add("selected");
     }
   }
@@ -143,27 +111,6 @@ function selectAnswer(choice, correctAnswer) {
   
       // Check if all levels have been completed
       if (level >= 10) {
-function validateAnswer() {
-  const choices = document.querySelectorAll(".choice");
-  let selectedAnswer;
-
-  choices.forEach((choice) => {
-    if (choice.classList.contains("selected")) {
-      selectedAnswer = choice.textContent;
-      }
-    });
-
-      if (selectedAnswer) {
-    //Retrieves the correct answer from API based on current level
-    // const correctAnswer = data.results[level - 1].correct_answer;
-
-    //Check if selected answer is correct
-        if (selectedAnswer === correctAnswer) {
-          level++;
-          document.getElementById("level").textContent = `Level ${level}`;
-
-      //Check if all levels have been completed
-      if (level > 10) {
         document.getElementById("question").textContent =
           "Congratulations! You passed all levels!";
 
@@ -191,53 +138,16 @@ function validateAnswer() {
       selectedChoice.classList.remove("selected");
     });
   }
-      //Increment strikes
-      strikes++;
-      document.getElementById("strikeCount").textContent = `Strikes: ${strikes}`;
-
-      //Checks if player has reached maximum strikes
-      if (strikes >= 3) {
-        //Display game over message
-        document.getElementById("question").textContent = "Game Over!";
-        return;
-      }
-      //Display strike message
-      document.getElementById("question").textContent =
-        "Incorrect answer. Try again.";
-
-      //Fetch new trivia question
-      getTrivia();
-    }
-  } else {
-    //No answer selected
-    console.log("Please select an answer");
-  }
-}
-
-function clearSelectedChoice() {
-  const selectedChoices = document.querySelectorAll(".choice.selected");
-  selectedChoices.forEach((selectedChoice) => {
-    selectedChoice.classList.remove("selected");
-  });
-}
 
   //Helper funtion to shuffle answers
   function shuffle(array) {
     let currentIndex = array.length;
     let temporaryValue;
     let randomIndex;
-//Helper funtion to shuffle answers
-// function shuffle(array) {
-//   let currentIndex = array.length;
-//   let temporaryValue;
-//   let randomIndex;
 
     while (currentIndex !== 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-//   while (currentIndex !== 0) {
-//     randomIndex = Math.floor(Math.random() * currentIndex);
-//     currentIndex -= 1;
 
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
@@ -246,4 +156,3 @@ function clearSelectedChoice() {
     return array;
   }
 });
-
